18edb66df7933c491622175b054df03f75a84e88
Esme <e02226427@gmail.com>
2025-12-23 13:07:28 +1300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ name = st.text_input("Whar is your name？")
 
 if name:
     st.write(f"hello，{name}！welcome to use my program 😊")
+st.title("声音测试")
+
+if st.button("点我"):
+    st.success("触发成功")
+    st.audio("sound.mp3")
+
 
 
